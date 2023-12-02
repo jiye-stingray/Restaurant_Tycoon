cd064587abd5ac89bf58371f5c94f24ae275241c
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-12-02 22:02:43 +0900

    🤩 l Quest BuyBtn Click

--- Restaurant_Tycoon/Assets/Scripts/Quest/QuestPanel.cs
+++ Restaurant_Tycoon/Assets/Scripts/Quest/QuestPanel.cs
@@ -8,13 +8,54 @@ public class QuestPanel : MonoBehaviour
     public TMP_Text _conditionText;
     public TMP_Text _buyBtnText;
 
-    [HideInInspector] public QuestData _questData;
+    private QuestData _questData;
 
-    public void SetPanel(QuestData _questData)
+    delegate void Quest();
+    Quest QuestDel;
+
+    public void SetPanel(QuestData questData)
     {
+        _questData = questData;
+
         _conditionText.text = _questData._questName;
         _buyBtnText.text = _questData._questCostValue.ToString();       // 나중에 방치형 금액 표시 로직으로 수정 필요  
+
+        QuestDel = null;
+
+        switch (_questData._questType)
+        {
+            case QuestType.Guest:
+                QuestDel += () => GuestQuest(_questData._questApplicationValue);
+                break;
+            case QuestType.Clerk:
+                break;
+            case QuestType.Price:
+                break;
+            case QuestType.Time:
+                break;
+            default:
+                break;
+        }
+
+        QuestDel += () => Destroy(gameObject);
     }
-    
+
+    /// <summary>
+    /// 구매 버튼 클릭
+    /// </summary>
+    public void BuyBtnClick()
+    {
+        if (_questData == null) return;
+
+        // 금액 조건 추가
+
+        QuestDel?.Invoke();
+
+    }
+
+    private void GuestQuest(int value)
+    {
+        Debug.Log(value);       // Temp
+    }
 
 }
--- Restaurant_Tycoon/Assets/Scripts/ScriptableObject/QuestData.cs
+++ Restaurant_Tycoon/Assets/Scripts/ScriptableObject/QuestData.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum QuestType
+{
+    Guest,          // 손님 추가
+    Clerk,          // 점원 추가
+    Price,          // 가격 추가
+    Time,           // 제작시간 줄이기
+
+}
+
 [CreateAssetMenu(fileName = "Quest", menuName = "Data/Quest")]
 public class QuestData : ScriptableObject
 {
     public string _questName;
-    public int _questValue;
+    public int _questCostValue;
+
+    public QuestType _questType;
+    public int _questApplicationValue;      // 적용 후 변경 값
 }
