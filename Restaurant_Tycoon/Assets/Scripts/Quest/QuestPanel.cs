eca28ec499b72f3d959acbf8b36d184c872510ad
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-12-05 12:21:46 +0900

    :sparkles: ㅣ Pointer Down, Up Event Trigger Add

--- Restaurant_Tycoon/Assets/Scripts/Quest/QuestPanel.cs
+++ Restaurant_Tycoon/Assets/Scripts/Quest/QuestPanel.cs
@@ -13,6 +13,8 @@ public class QuestPanel : MonoBehaviour
     delegate void Quest();
     Quest QuestDel;
 
+    bool _isBtnDown;        // 버튼을 누르고 있는가
+
     public void SetPanel(QuestData questData)
     {
         _questData = questData;
@@ -57,5 +59,16 @@ public class QuestPanel : MonoBehaviour
     {
         Debug.Log(value);       // Temp
     }
+
+    public void BtnDown()
+    {
+        _isBtnDown = true;
+    }
+
+    public void BtnUp()
+    {
+        _isBtnDown = false;
+    }
+
 
 }
