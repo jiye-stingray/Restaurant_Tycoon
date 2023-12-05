1a40ca3a6683a9a0a23397c04e5fba02b12b72df
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-12-05 15:13:38 +0900

    :sparkles: ㅣ level up btn sprite color change

--- Restaurant_Tycoon/Assets/Scripts/FoodLevelUpBtn.cs
+++ Restaurant_Tycoon/Assets/Scripts/FoodLevelUpBtn.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FoodLevelUpBtn : MonoBehaviour
 {
     [SerializeField] bool _isBtnDown;
+    [SerializeField] Image _sprite;
 
     private async void LevelUp()
     {
         while(_isBtnDown)
         {
+
+            _sprite.color = Color.black;
             Debug.Log("Level Up");
-            await Task.Delay(500);
+            await Task.Delay(200);
+            _sprite.color = Color.white;
+            await Task.Delay(300);
 
         }
     }
